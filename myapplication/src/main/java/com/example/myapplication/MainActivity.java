fbdfc41780cd4541b9925493eb7552ddb36d2312
jiabailong <jiabailong@100tal.com>
2021-03-04 10:26:14 +0800

    日志拼接到本地

--- myapplication/src/main/java/com/example/myapplication/MainActivity.java
+++ myapplication/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,18 +1,17 @@
-package com.example.nativecrashcatching;
+package com.example.myapplication;
 
 import android.Manifest;
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
+import com.example.nativecrashcatching.CrashCatching;
 
 import java.io.File;
-import java.security.Permission;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,15 +20,12 @@ public class MainActivity extends Activity {
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
 
         // Example of a call to a native method
-        TextView tv = findViewById(R.id.sample_text);
+        TextView tv = findViewById(R.id.test);
         tv.setText("Hello, World");
         CrashCatching.init();
 
         tv.setOnClickListener(v -> {
             CrashCatching.die();
-            File file=new File("/sdcard/a.txt");
-            Log.e("@ati",file.exists()+"--fileStatus");
         });
     }
-
 }
